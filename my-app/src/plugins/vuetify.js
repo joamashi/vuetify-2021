74340430fac327329a72e7caa0a1e5d11bf664ae
devux.ai <notenore@gmail.com>
2021-04-11 23:13:32 +0900

    no message

--- my-app/src/plugins/vuetify.js
+++ my-app/src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-
+import colors from 'vuetify/lib/util/colors'
 Vue.use(Vuetify);
 
 export default new Vuetify({
@@ -11,6 +11,16 @@ export default new Vuetify({
         secondary: '#b0bec5',
         accent: '#8c9eff',
         error: '#b71c1c',
+        // primary: '#1976D2',
+        // secondary: '#424242',
+        // accent: '#82B1FF',
+        // error: '#FF5252',
+        // info: '#2196F3',
+        // success: '#4CAF50',
+        // warning: '#FFC107',
+      },
+      dark: {
+        primary: colors.blue.lighten3,
       },
     },
   },
